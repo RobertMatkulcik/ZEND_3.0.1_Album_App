aa5f3972c5c5ed3e02dac0d548eaa430ead6b3b6
Robert Matkulcik <robert@matkulcik.eu>
2016-08-23 17:09:17 +0200

    ADD new album

--- skeleton-application/module/Album/src/Form/AlbumForm.php
+++ skeleton-application/module/Album/src/Form/AlbumForm.php
@@ -5,3 +5,47 @@
  * Date: 8/23/16
  * Time: 10:15 AM
  */
+
+namespace Album\Form;
+
+use Zend\Form\Form;
+
+//Default is ZEND uncoment for GET
+//$this->setAttribute('method', 'GET');
+
+
+class AlbumForm extends Form
+{
+	public function __construct($name = null)
+	{
+		// We will ignore the name provided to the constructor
+		parent::__construct('album');
+
+		$this->add([
+			'name' => 'id',
+			'type' => 'hidden',
+		]);
+		$this->add([
+			'name' => 'title',
+			'type' => 'text',
+			'options' => [
+				'label' => 'Title',
+			],
+		]);
+		$this->add([
+			'name' => 'artist',
+			'type' => 'text',
+			'options' => [
+				'label' => 'Artist',
+			],
+		]);
+		$this->add([
+			'name' => 'submit',
+			'type' => 'submit',
+			'attributes' => [
+				'value' => 'Go',
+				'id'    => 'submitbutton',
+			],
+		]);
+	}
+}
